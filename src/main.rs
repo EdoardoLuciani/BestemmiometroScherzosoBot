76a6f1e6867ec8bf5c3fcafb2b12e231a18c65d4
EdoardoLuciani <edoardo.luciani@gmail.com>
2023-03-04 18:23:45 +0000

    Run example bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+use dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_message(msg.chat.id, "Shut up").await?;
+        Ok(())
+    })
+    .await;
 }
